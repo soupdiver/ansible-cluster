40da84fc60744f4b0f818932254907a306e2225d
Felix <felix@psy-coding.com>
2014-06-05 11:01:49 +0200

    added bash script for spinning up machines with keys

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+for i in {1..5}
+do
+  #check if key folder exists for the machine
+  if [ ! -d "keys/m$i" ]; then
+      mkdir -p "keys/m$i"
+      echo "created key folder for machine $i"
+  fi
+  
+  #check if key exsists for the machine
+  if [ ! -f "keys/m$i/id_rsa" ]; then
+      ssh-keygen -t rsa -b 4096 -C "machine$i" -f "keys/m$i/id_rsa"
+      echo "created key for machine $i"
+    else
+      echo "machine $i already got a key"
+  fi
+  
+  cd "machines/m$i"
+  vagrant destroy -f
+  vagrant up
+  cd ..
+  cd ..
+done
