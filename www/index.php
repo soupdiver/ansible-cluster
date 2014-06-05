86cf7aa3333e50c1d7e27b07ab2d622bbaea7b16
Felix <felix@psy-coding.com>
2014-06-05 11:02:03 +0200

    removed old www content

--- www/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Pragma: no-cache');
-header('Cache-Control: private, no-cache, no-store, max-age=0, must-revalidate, proxy-revalidate');
-
-echo $_SERVER["SERVER_ADDR"];
-
-phpinfo();
